d6aa06cf0bf2dd74c93ff63699d75b7e63f22f1c
David <39740665+s3rvant@users.noreply.github.com>
2025-01-24 12:51:52 -0500

    Initial function to generate a random result

--- randomRealitiesTables.js
+++ randomRealitiesTables.js
@@ -1,3 +1,4 @@
+// Object with each of the form sections as objects with each of that section's entries and their potential results
 const OPTIONS = {
     
     // FATE Tables
@@ -264,3 +265,30 @@ const OPTIONS = {
         ]
     }
 };
+
+// Roll on arrays and return em dashed result
+function getRandomResult(section, entry){
+
+    // Validate parameters
+    if(!OPTIONS.hasOwnProperty(section)){
+        return 'Invalid Section';
+    }
+    if(!OPTIONS[section].hasOwnProperty(entry)){
+        return 'Invalid Entry';
+    }
+
+    // Loop over each array within the entry to build the result
+    var parts = OPTIONS[section][entry].length;
+    var result = '';
+    for(var part = 0; part < parts; part++){
+
+        // Roll on individual result array and add to our final result
+        var roll = Math.floor(Math.random() * OPTIONS[section][entry][part].length);
+
+        // Add the rolled part to the result
+        result += OPTIONS[section][entry][part][roll] + '—';
+    }
+
+    // Remove the ending em dash and return the result
+    return result.slice(0, -1);
+}
